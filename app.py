8fac7e9b5a03f7198882e621b93198fee1d3e599
xiaozhihxx <894385370@qq.com>
2025-01-15 10:05:11 +0800

    first commit

--- app.py
+++ app.py
@@ -2,67 +2,6 @@ from flask import Flask, render_template, request, jsonify, Response
 from test import con_my_sql
 
 app = Flask(__name__)
-
-# 入口
-@app.route('/')
-def hello_world():  # put application's code here
-    # return 'Hello World!'
-    return render_template('index.html')
-
-@app.route('/add')
-def add():  # put application's code here
-    # return 'Hello World!'
-    return render_template('register.html')
-
-# 注册
-@app.route('/register', methods=['post'])
-def register():
-    # name = request.form.get('username')
-    # pwd = request.form.get('password')
-
-    name = request.json.get('username')
-    pwd = request.json.get('password')
-
-    # 查询用户是否存在
-    code = 'select * from users where username=%s'
-    cursor_ans = con_my_sql(code, (name))
-    cursor_select = cursor_ans.fetchall()
-
-    if len(cursor_select) > 0:
-        return '用户已存在'
-    else:
-        login_data[name] = pwd
-        insertCode = 'INSERT INTO users (username, password) VALUES (%s, %s)'
-        insertResult = con_my_sql(insertCode, (name, pwd))
-        print(insertResult, 'BBBB')
-        return '注册成功'
-
-# 登录数据字典
-login_data = {
-    '张三': 123456
-}
-
-# 登录
-@app.route('/login', methods=['post'])
-def login():
-    name = request.form.get('username')
-    pwd = request.form.get('password')
-
-    code = 'select * from users where username=%s'
-    cursor_ans = con_my_sql(code, (name))
-    cursor_select = cursor_ans.fetchall()
-    print(cursor_select,'aaaa')
-
-    if len(cursor_select) > 0:
-        if pwd == cursor_select[0]['password']:
-            return '登录成功'
-        else:
-            return '密码错误'
-    else:
-        return '用户不存在'
-
-@app.route('/query', methods=['get'])
-def query():
     # 接受url ？后的参数
     # param = request.args.get('param')
     # print(param, 'aaaa')
@@ -71,26 +10,30 @@ def query():
     cursor_select = cursor_ans.fetchall()
     return jsonify(cursor_select)
 
-# 校验【有人】推送地址校验，返回
-@app.route('/check', methods=['get', 'post'])
-def check():
+# 【有人】推送数据
+# 根组织
+@app.route('/receiveRootGroup', methods=['get', 'post'])
+def receiveRootGroup():
     if request.method == 'GET':
         verify = request.args.get('verify')
-        print(verify, 'get check')
+        print(verify, 'get receiveRootGroup')
         return verify
     elif request.method == 'post':
         raw_data = request.data
-        print(raw_data, 'post check')
+        print(raw_data, 'post receiveRootGroup')
         return Response(status=200)
 
-# 接收【有人】http数据的推送
-@app.route('/receive', methods=['post'])
-def receive():
-    raw_data = request.data
-    print(raw_data, 'receive data')
-    return ''
-
-
+# 鳜鱼池
+@app.route('/receiveJueyu', methods=['get', 'post'])
+def receiveJueyu():
+    if request.method == 'GET':
+        verify = request.args.get('verify')
+        print(verify, 'get receiveJueyu')
+        return verify
+    elif request.method == 'post':
+        raw_data = request.data
+        print(raw_data, 'post receiveJueyu')
+        return Response(status=200)
 
 if __name__ == '__main__':
     # 调试模式，html模板变动，页面也会跟着变。不需要重新启动服务
